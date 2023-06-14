c7c48dcd16426c2743b2f58124c18ba32f452e3f
Hai-Dang Huynh-Lam <danghailamhuynh@gmail.com>
2023-06-15 01:09:05 +0700

    Move video visualization to eval

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -7,34 +7,6 @@ import matplotlib.pyplot as plt
 import matplotlib.offsetbox as offsetbox
 
 from model.reducer import Reducer
-from model.visualizer import broadcast_video
-
-
-def visualize_video(video_folder, embedding_folder, clustering_folder,
-                    demo_folder, video_name, frag_width, fps=None):
-    sample_file = os.path.join(embedding_folder, f'{video_name}_samples.npy')
-    keyframe_file = os.path.join(clustering_folder, f'{video_name}_keyframes.npy')
-    
-    sample_video_path = os.path.join(demo_folder, f'{video_name}_sample.avi')
-    keyframe_video_path = os.path.join(demo_folder, f'{video_name}_keyframes.avi')
-    raw_video_path = os.path.join(video_folder, f'{video_name}.mp4')
-    
-    try:
-        samples = np.load(sample_file)
-        broadcast_video(input_video_path=raw_video_path, frame_indices=samples,
-                        output_video_path=sample_video_path,
-                        fragment_width=frag_width, fps=fps
-                        )
-        
-        keyframes = np.load(keyframe_file)
-        broadcast_video(input_video_path=raw_video_path,
-                        frame_indices=keyframes,
-                        output_video_path=keyframe_video_path,
-                        fps=fps, fragment_width=frag_width
-                        )
-    except Exception as error:
-        print(error)
-        print(f'{video_name} not found')
 
 
 def visualize_cluster(video_folder, embedding_folder,
@@ -107,14 +79,14 @@ def visualize_cluster(video_folder, embedding_folder,
     # except Exception as e:
     #   print(e)
 
-  
+
 def main():
     parser = argparse.ArgumentParser(description='Visualize result')
     parser.add_argument('--video-folder', type=str, required=True,
                         help='Path to folder containing videos')
     parser.add_argument('--embedding-folder', type=str, required=True,
                         help='path to folder containing feature files')
-    parser.add_argument('--clustering-folder', type=str, required=True,
+    parser.add_argument('--context-folder', type=str, required=True,
                         help='path to output folder for clustering')
     parser.add_argument('--demo-folder', type=str, required=True,
                         help='path to folder saving demo videos')
@@ -135,22 +107,13 @@ def main():
 
     args = parser.parse_args()
     
-    if args.visual_type == 'cluster':
-        visualize_cluster(video_folder=args.video_folder,
-                          embedding_folder=args.embedding_folder,
-                          context_folder=args.clustering_folder,
-                          video_name=args.video_name,
-                          show_image=args.show_image,
-                          color_value=args.color_value
-                          )
-    else:
-        visualize_video(video_folder=args.video_folder,
-                        embedding_folder=args.embedding_folder,
-                        clustering_folder=args.clustering_folder,
-                        demo_folder=args.demo_folder,
-                        video_name=args.video_name,
-                        frag_width=args.frag_width,
-                        fps=args.output_fps)
+    visualize_cluster(video_folder=args.video_folder,
+                      embedding_folder=args.embedding_folder,
+                      context_folder=args.context_folder,
+                      video_name=args.video_name,
+                      show_image=args.show_image,
+                      color_value=args.color_value
+                      )
 
 
 if __name__ == '__main__':
